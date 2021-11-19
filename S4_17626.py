7c1c8450cdcb82c82deaa9e63116db23bb3b5b8f
tjdwns8793 <tjdwns8793@mensakorea.org>
2021-11-19 10:59:13 +0900

    S4_17626, dp, bruteForce

---
+++ S4_17626.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dp = [4 for i in range(n+1)]
+dp[0] = 0
+
+for i in range(1, n+1):
+    for j in range(1, int(i**(1/2)) + 1):
+        if dp[i] > dp[i - j**2] + 1:
+            dp[i] = dp[i - j**2] + 1
+
+print(dp[n])
+
+
+
+# 50000**(1/2)=223.61
