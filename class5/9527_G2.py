3f4d2cd0d8c976facf46014f0b3d2cb6e735fc91
tjdwns8793 <tjdwns8793@mensakorea.org>
2022-05-29 10:41:25 +0900

    G2_9527, N까지의 binary 1의 개수 세기

---
+++ class5/9527_G2.py
@@ -0,0 +1,18 @@
+A, B = map(int, input().split())
+    
+def OneOfNum(N):
+    save = 0
+    ans = 0
+    storage = list(bin(N)[2:])
+    L = len(storage)
+    for i in range(L-1):
+        if storage[i] == "1":
+            ans += (L-i-1)*(2**(L-i-2))+save*(2**(L-i-1))
+            save += 1
+    if storage[L-1] == "1":
+        ans += save*2 + 1
+    else:
+        ans += save
+    return ans
+
+print(OneOfNum(B) - OneOfNum(A-1))
