2fa9bca7feedd1c143be9308c816bc7ec76700e9
SeongJun-Ham <seongjun.ham.dev@gmail.com>
2025-12-10 20:46:47 +0900

    1167, 트리의 지름, dfs 두 번, 트리는 isolated vertex가 없음

--- class4/1167_G2.py
+++ class4/1167_G2.py
@@ -1,32 +1,39 @@
 import sys
-sys.setrecursionlimit(100001)
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
-def dfs(edgeVal, bef, cur, index, dis):
-    if len(edgeVal[cur]) == index or (len(edgeVal[cur]) == 1 and edgeVal[cur][0][0] == bef):
-        global max_distance
-        global endEdge
-        if max_distance < dis:
-            max_distance = dis
-            endEdge = cur
-        return
+def dfs(cur_v, dis):
+    global max_distance
+    global longest_vertex
+    if dis > max_distance:
+        max_distance = dis
+        longest_vertex = cur_v
+    
+    for next_v in edgeValue[cur_v]:
+        if visited[next_v[0]]:
+            continue
+        visited[next_v[0]] = 1
+        dfs(next_v[0], dis+next_v[1])
+        visited[next_v[0]] = 0
 
-    if edgeVal[cur][index][0] != bef:
-        dfs(edgeVal, cur, edgeVal[cur][index][0], 0, dis+edgeVal[cur][index][1])
 
-    dfs(edgeVal, bef, cur, index+1, dis)
+if __name__ == "__main__":
+    V = int(input())
+    edgeValue = [[] for _ in range(V+1)]
+    for _ in range(V):
+        temp = tuple(map(int, input().split()))
+        for i in range(1, len(temp)-1, 2):
+            edgeValue[temp[0]].append((temp[i], temp[i+1]))
 
-if __name__ == '__main__':
-    N = int(sys.stdin.readline())
-    edgeValue = [[] for _ in range(N+1)]
-    for _ in range(N):
-        inp = tuple(map(int, sys.stdin.readline().split()))
-        for i in range(1, len(inp)-1, 2):
-            edgeValue[inp[0]].append((inp[i], inp[i+1]))
-
+    visited = [0]*(V+1)
     max_distance = 0
-    endEdge = 0
-    dfs(edgeValue, 0, 1, 0, 0)
-    max_distance = 0
-    dfs(edgeValue, 0, endEdge, 0, 0)
+    longest_vertex = 0
+
+    visited[1] = 1
+    dfs(1, 0)
+
+    visited = [0]*(V+1)
+    visited[longest_vertex] = 1
+    dfs(longest_vertex, 0)
 
     print(max_distance)
